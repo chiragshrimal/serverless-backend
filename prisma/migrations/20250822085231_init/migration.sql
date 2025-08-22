09fec33af4d215ef561149d780e625f8a981e5eb
chiragshrimal <chirag1711shrimal@gmail.com>
2025-08-22 14:51:08 +0530

    create conn with database and connection pooling form prismna accelerate

---
+++ prisma/migrations/20250822085231_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,45 @@
 // and we dont required to write app.listen(3000) something like this 
+// insted of express use hono in the serverless 
 import { Hono } from 'hono'
+
+// from the prisma Accelerate 
+// for pooling connection 
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+import {env} from 'hono/adapter';
 
 const app = new Hono();
 
-app.post("/api/v1/signup", async (c)=>{
-  const body= await c.req.json();
-  console.log(body);
-  console.log(c.req.query());
-  console.log(c.req.header("Authorization"));
-  // console.log(c.req.param())
-  // console.log(c.req,)
+app.post("/",async(c)=>{
+  // Todo add zod validation here 
+  const {name, email, password}=await c.req.json();
+  
+  if(!name || !email || !password){
+    return c.json({
+      message : "please provide all the entries"
+    })
+  }
+
+  // take database url from the env 
+  const {DATABASE_URL}=env<{DATABASE_URL:string}>(c)
+
+  //  create prisma client from the that DATABASE_URL
+  const prisma = new PrismaClient({
+    datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  // create the user in the database 
+  await prisma.user.create({
+    data:{
+      name,
+      email,
+      password
+    }
+  });
+
   return c.json({
-    message : "User is chirag"
+    message : "user is created"
   })
-});
 
-app.post("/api/v1/signin",(c)=>{
-  return c.json({
-    message : "User is chirag"
-  })
-});
-
-
-app.post("/api/v1/todo",(c)=>{
-  return c.json({
-    message : "User is chirag"
-  })
-});
-
-export default app
+})
